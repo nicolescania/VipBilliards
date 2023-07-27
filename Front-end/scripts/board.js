11fa5b5422c4d63741be36a04ba70732ce11e60a
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-07-27 16:25:31 -0400

    added modal to games

--- Front-end/scripts/board.js
+++ Front-end/scripts/board.js
@@ -19,13 +19,17 @@ async function displayGameList() {
     games = await getGameList()
     console.log(games)
     games.forEach(game => {
+        let gameName = game.name
 
         const row = document.getElementById('row');
         card = document.createElement('div');
         card.classList = "col-4 mb-2";
         card.innerHTML = `
-        <div class="card text-center py-4"> ${game.name}</div>      
-    `;
+     
+        <div class="card text-center py-4"> 
+        <a href="javascript:getGame('${game._id}', '${gameName}')"> ${game.name}</a> 
+        </div>       
+     `;
 
         row.appendChild(card);
 
@@ -34,20 +38,18 @@ async function displayGameList() {
 }
 
 
-    async function getGameId() {
-     
-    }
+async function getGame(gameid, gameName) {
+
+    document.getElementById('gameName').innerHTML = `${gameName}`;
+    document.getElementById('gameModal').innerHTML = `<a href="javascript:startGame('${gameid}')"> Start </a>`;
+
+
+}
 
 
 
-async function startGame() {
-    games = await getGameList()
-    
-    games.forEach(game => {
-        gameId = game._id
-       return  gameId
-    
-    })
+async function startGame(gameId) {
+
 
     try {
 
@@ -62,20 +64,22 @@ async function startGame() {
 
 }
 
-startGame()
-
-async function initialGame() {
-    alert(startGame)
-}
-
-
-const container = document.getElementById("container");
-container.addEventListener("click", initialGame);
-
-
-
-
-
 displayGameList()
 
+//CREAR EL MODAL EN HTML (COPIAR Y PEGAR DESDE LA DOCUMENTACION):
+    //A. COLOCARLE ALGUN IDENTIFICADOR A LA ETIQUETA DEL NOMBRE, ID UNICO, CLASE, ETC
+    //B. COLOCARLE IDENTIFICADOR A ALGUN DIV
+
+//AL DAR CLICK A LA MESA, LLAMAR UNA FUNCIÓN QUE RECIBA DOS PARAMETROS: GAMEID Y GAMENAME
+    //A. LA FUNCION VA A LLAMAR EL IDENTIFICADOR DE LA ETIQUETA DEL NOMBRE Y PONERLE EL NOMBRE DEL JUEGO SELECCIONADO
+    //B. AL DIV VACIO, LE VAS A COLOCAR UN INNERHTML CON UN BOTON QUE DIGA "START" Y QUE LLAME LA FUNCION STARTGAME
+
+//ABRIR EL MODAL
+
+
+
+
+
+
+       // <a href="javascript:startGame('${game._id}')"> ${game.name}</a> 
 
