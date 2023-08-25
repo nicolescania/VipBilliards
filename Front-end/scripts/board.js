b5f63323589e1c01495a12502e316e23fa95d116
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-08-25 00:53:04 -0400

    finished transfer table

--- Front-end/scripts/board.js
+++ Front-end/scripts/board.js
@@ -1,6 +1,7 @@
 
 
 
+displayGameList()
 // GET LIST OF GAMES
 async function getGameList() {
 
@@ -14,93 +15,149 @@ async function getGameList() {
 
 // DISPLAY ALL THE GAMES IN THE SCREEN
 async function displayGameList() {
- 
+
     games = await getGameList()
     //console.log(games)
-    games.forEach(game => {
-         console.log(game)
+    games.forEach(async (game) => {
+        console.log(game)
         let gameName = game.name
+
+
 
         const row = document.getElementById('row');
         card = document.createElement('div');
         card.classList = "col-4 mb-2";
         card.innerHTML = `      
         <div class="card bg-body-secondary text-center py-4 m-2" id= '${game._id}'> 
-        <a  class="text-decoration-none text-dark fs-1 fw-bold p-2 m-2 "  id= '${gameName}' href="javascript:getGame('${game._id}', '${gameName}') "> ${gameName} </a> 
-        <a  class="text-decoration-none"  id= 'disponibilidad' > AVAILABLE </a>
+        <a  class="text-decoration-none text-dark fs-1 fw-bold p-2 m-2 "  id= '${gameName}' href="javascript:getGame('${game._id}', '${gameName}') "> ${gameName} </a>
+   
         
         </div>       
      `;
-         verifyGameActive(game._id)
+        verifyGameActive(game._id)
 
         row.appendChild(card);
- 
+
     });
 }
+
 
 
 // VERIFY GAMES ACTIVE
 
 async function verifyGameActive(gameid) {
 
-   getGameActive(gameid)
-  
+    getGameActive(gameid)
+
 }
+
+
+// GET GAME ACTIVE
+
+async function getGameActive(gameId) {
+
+    try {
+
+        bodyInfo = { 'gameId': gameId }
+        const response = await getRequest(`${URL}/game/game-active`, `POST`, bodyInfo);
+        data = await response.json();
+
+        if (data.gameStatus == true) {
+            changeColor(gameId, data.game)
+
+            return {
+                status: 'TAKEN',
+                isAvailable: false,
+                timeStarted: data.gameStarted,
+                payment: data.charge
+            }
+        } if (data.gameStatus == false) {
+            changeHoldColor(gameId, data.game)
+            return {
+                status: 'ON HOLD',
+                isAvailable: false,
+                timeStarted: data.gameStarted,
+                payment: data.charge
+            }
+        }
+        return {
+            status: 'AVAILABLE',
+            isAvailable: true,
+
+        }
+
+    } catch {
+        return "Error en el get";
+    }
+
+}
+
+
+
+function showModal() {
+    var modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+    modal.show();
+    
+}
+
+
+
 
 
 
 // GET THE GAME STARTED 
 async function getGame(gameid, gameName) {
-    const result = await getGameActive(gameid)
-    document.getElementById('gameInfo').innerHTML =  `<p>Table '${gameName}'  '${result.status}' .</p>`;
+ 
+    showModal()
     document.getElementById('gameName').innerHTML = `${gameName}`;
+
+    const data = await getGameActive(gameid)
+    console.log(data)
+    if (data) {
+        document.getElementById('gameInfo').innerHTML = `<p>Table '${gameName}'  '${data.status}'.</p>
+        <p>Time Started '${data.timeStarted}' .</p>
+        <p> payment '${data.payment}' .</p>
+        `;
+    }
+
+
     myModal = document.getElementById('gameModal').innerHTML = `<a href="javascript:startGame('${gameid}') "> Start </a>`;
     myModal = document.getElementById('holdButtonModal').innerHTML = `<a href="javascript:holdGame('${gameid}') "> Hold </a>`;
     myModal = document.getElementById('resumeButtonModal').innerHTML = `<a href="javascript:resumeGame('${gameid}') "> Resume </a>`;
     myModal = document.getElementById('closeButtonModal').innerHTML = `<a href="javascript:closeGame('${gameid}') "> Close </a>`;
-    myModal = document.getElementById('transferModal').innerHTML = `<a href="javascript:TransferModal() "> Transfer </a>`;
+    myModal = document.getElementById('transferModal').innerHTML = `<a href="javascript:TransferModal('${gameid}') "> Transfer </a>`;
 
- 
   
 }
 
 
-async function TransferModal(){
-        
- 
+async function TransferModal(gameid) {
+
+  
+
+  
+
+    var modal = new bootstrap.Modal(document.getElementById("transfermodal02"));
+    modal.show();
     games = await getGameList()
 
-    games.forEach( async (game) => {
-      
-        const result = await getGameActive(game._id)
-        
-      if   (  result.isAvailable == true )
-      {
+    const select = document.getElementById('menuSelectionGames');
+    select.innerHTML = ''
+    games.forEach(async (game) => {
+
+        const data = await getGameActive(game._id)
+        if (data.isAvailable == true) {
+            let gameName = game.name
+            console.log(game)
+            select.innerHTML = select.innerHTML + `<option value="${game._id}">${gameName}</option>`;
+
+        }
 
 
-        let gameName = game.name
-        console.log(game)
 
+    });
+    document.getElementById('transferGameModal').innerHTML = `<a href="javascript:transferGame('${gameid}') "> Transfer </a>`;
 
-        const select = document.getElementById('menuSelectionGames');
-        option = document.createElement('option');
-        //option.classList = "col-4 mb-2";
-        option.innerHTML = `  
-        <option value="1">${gameName}</option>
- 
-    
-     `;
-
-        select.appendChild(option);
-
-            
-      }
-  
- 
-   // myModal = document.getElementById('transferModal').innerHTML = `<a href="javascript:transferGame('${gameid}') "> Close </a>`;
-   // document.getElementById('gameavailable').innerHTML = `${gameName}`;
-
-});
 }
 
 
@@ -121,22 +178,21 @@ function changeColor(gameid, gameName) {
     // Add the new class
     myDiv.classList.add("bg-info");
 
-        // Remove the class
-        text.classList.remove("text-dark");
+    // Remove the class
+    text.classList.remove("text-dark");
 
-        // Add the new class
-        text.classList.add("text-white");
-    
+    // Add the new class
+    text.classList.add("text-white");
 
 
-        
+
+
 
 
 }
 
 
-function changeHoldColor(gameid, gameName)
-{
+function changeHoldColor(gameid, gameName) {
     const myDiv = document.getElementById(gameid)
     const text = document.getElementById(gameName)
 
@@ -147,12 +203,12 @@ function changeHoldColor(gameid, gameName)
     // Add the new class
     myDiv.classList.add("bg-warning");
 
-      // Remove the class
-      text.classList.remove("text-white");
+    // Remove the class
+    text.classList.remove("text-white");
 
-      // Add the new class
-      text.classList.add("text-dark");
-  
+    // Add the new class
+    text.classList.add("text-dark");
+
 
 }
 
@@ -232,57 +288,21 @@ async function resumeGame(gameId) {
 }
 
 // TRANSFER GAME
-async function transferGame(gameIdOld,gameIdNew ) {
+async function transferGame(gameIdOld) {
     try {
 
-        bodyInfo = { 'gameId01': gameIdOld, 'gameId02': gameIdNew  }
+        const gameIdNew = document.getElementById("menuSelectionGames").value
+
+        bodyInfo = { 'gameId01': gameIdOld, 'gameId02': gameIdNew }
         const response = await getRequest(`${URL}/game/transfer-game`, `POST`, bodyInfo);
         data = await response.json();
 
-        changeColor(gameId)
+        changeColor(gameIdNew)
 
         return console.log(data);
 
     } catch {
-
-    }
-
-}
-
-
-
-
-
-// GET GAME ACTIVE
-
-async function getGameActive(gameId) {
-
-    try {
-
-        bodyInfo = { 'gameId': gameId }
-        const response = await getRequest(`${URL}/game/game-active`, `POST`, bodyInfo);
-        data = await response.json();
-        if (data.gameStatus == true) {
-            changeColor(gameId, data.game) 
-            
-            return {
-                status: 'TAKEN',
-                isAvailable: false
-               }
-        }if(data.gameStatus == false) {
-            changeHoldColor(gameId,data.game) 
-            return {
-                status: 'ON HOLD',
-                isAvailable: false
-               }
-        } 
-            return {
-                status: 'AVAILABLE',
-                isAvailable: true
-               }
-
-    } catch {
-
+       return console.log('no funciona transfer game func')
     }
 
 }
@@ -292,7 +312,6 @@ async function getGameActive(gameId) {
 
 
 
-displayGameList()
 
 
 
@@ -301,28 +320,9 @@ displayGameList()
 
 
 
-// Get the div element by its ID
-const myDiv = document.getElementById("mydiv");
-
-// Add a click event listener to the div element
-myDiv.addEventListener("click", function () {
-    // Change the background color of the div when it's clicked
-    myDiv.innerHTML = `
-     
-  <div class="card bg-black text-center py-4"> 
-
-  </div>       
-
-// `
-
-    // Save the change to local storage
-    localStorage.setItem('myChange', 'Your changed value');
-
-    // Retrieve the change from local storage
-    const myChange = localStorage.getItem('myChange');
-    console.log(myChange); // Output: Your changed value
 
 
 
-});
+
+
 
